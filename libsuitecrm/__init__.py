24d92997ebea4bd4663b5ef94c9f87308db1e41a
Chris Arridge <chris.arridge@opendataservices.coop>
2025-10-06 11:00:17 +0100

    fix: add missing exceptions to export list in module __init__.py

--- libsuitecrm/__init__.py
+++ libsuitecrm/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 from .crm import SuiteCRM
-from .exceptions import CannotUnderstandResponse, RequestFailed
+from .exceptions import CannotUnderstandResponse, RequestFailed, AuthorisationFailed, CreateRecordFailed, UpdateRecordFailed, DeleteRecordFailed, CreateRelationshipFailed
 from .filter import Filter
 
 __all__ = [
